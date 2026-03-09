06c25bd1dd9220cdaa11d2300974943ec836735f
liuyu <liuyu0123@outlook.com>
2026-03-09 11:40:39 +0800

    modify run.sh(add test), fix bug in requirements.txt

--- run.sh
+++ run.sh
@@ -16,3 +16,12 @@ python Train_SegNet_Pavements.py \
     "D:\Files\Data\USVInlandDataset\Water Segmentation\training\training\640_320_undistorted_gif" \
     ./logs \
     --weight-fn weights/water_seg.pth.tar
+
+#测试模型
+#Powershell 格式
+# 先 cd 到 WaterSegment 文件夹
+python Test_SegNet_Pavements.py `
+    "D:\Files\Data\USVInlandDataset\Water Segmentation\training\training\640_320_undistorted" `
+    "D:\Files\Data\USVInlandDataset\Water Segmentation\training\training\640_320_undistorted_gif" `
+    ".\weights\checkpoint_pavements_20260309_111733.pth.tar" `
+    ".\results"
